951c845694fb7bb6dbf3b8fa6bf4b32d8ed3c627
Paul Pekala <ppekala@ford.com>
2021-02-06 15:55:55 -0500

    started adding http get request

--- APM/APM-Start/src/app/app.module.ts
+++ APM/APM-Start/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';  
 import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { StarComponent } from './shared/star.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   bootstrap: [AppComponent]
 })
--- APM/APM-Start/src/app/products/product.service.ts
+++ APM/APM-Start/src/app/products/product.service.ts
@@ -1,33 +1,49 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class ServiceNameService {
+    constructor(private httpClient: HttpClient) { }
+    
+}
 
 @Injectable({
     providedIn: 'root',
 })
 export class ProductService {
 
-    getProducts(): IProduct[] {
-        return [
-            {
-                "productId": 2,
-                "productName": "Garden Cart",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2019",
-                "description": "jfkl;asdfjas;k",
-                "price": 32.99,
-                "starRating": 4.2,
-                "imageUrl": "assets/images/garden_cart.png"
-            },
-            {
-                "productId": 3,
-                "productName": "Hammer",
-                "productCode": "TBX-0023",
-                "releaseDate": "April 20, 2020",
-                "description": "jfk;k",
-                "price": 69.69,
-                "starRating": 8.4,
-                "imageUrl": "assets/images/hammer.png"
-            }
-        ];
+    private productUrl = 'www.myWebService.com/api/products';
+
+    constructor(private http: HttpClient) { }
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl);
     }
+
+    // getProducts(): IProduct[] {
+    //     return [
+    //         {
+    //             "productId": 2,
+    //             "productName": "Garden Cart",
+    //             "productCode": "GDN-0023",
+    //             "releaseDate": "March 18, 2019",
+    //             "description": "jfkl;asdfjas;k",
+    //             "price": 32.99,
+    //             "starRating": 4.2,
+    //             "imageUrl": "assets/images/garden_cart.png"
+    //         },
+    //         {
+    //             "productId": 3,
+    //             "productName": "Hammer",
+    //             "productCode": "TBX-0023",
+    //             "releaseDate": "April 20, 2020",
+    //             "description": "jfk;k",
+    //             "price": 69.69,
+    //             "starRating": 8.4,
+    //             "imageUrl": "assets/images/hammer.png"
+    //         }
+    //     ];
+    // }
 }
